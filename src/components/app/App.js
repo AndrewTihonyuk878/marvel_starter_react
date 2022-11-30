faf1cd0eda6d05b19a25f42708709f5f580252fd
Andrii <andrew.tihonyuk878@gmail.com>
2022-11-30 14:18:22 +0100

    charlist

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,24 +1,38 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
+import ComicsList from "../comicsList/ComicsList";
 
-const App = () => {
+class App extends Component{
+    state = {
+        choicedChar: null
+    }
+
+    onCharChoiced = (id) => {
+        this.setState({
+            choicedChar: id
+        })
+    }
+
+   render() {
     return (
         <div className="app">
             <AppHeader/>
             <main>
                 <RandomChar/>
                 <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
+                    <CharList onCharChoiced={this.onCharChoiced}/>
+                    <CharInfo charId={this.state.choicedChar}/>
                 </div>
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
     )
+   }
 }
 
 export default App;
