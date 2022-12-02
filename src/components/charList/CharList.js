2d141401c6b2ae9554a8e3529b886a2363dedf2e
Andrii <andrew.tihonyuk878@gmail.com>
2022-12-02 08:56:47 +0100

    activeclass

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -12,22 +12,44 @@ class CharList extends Component {
     state = {
         charList: [],
         loading: true,
-        error: false
+        error: false,
+        newItemsLoading: false,
+        offset: 1551,
+        charEnded: false
     }
 
     marvelService = new MarvelService
 
     componentDidMount() {
-        this.marvelService.getAllCharacters()
+        this.onRequest()
+    }
+
+    onRequest = (offset) => {
+        this.onCharListLoading()
+        this.marvelService.getAllCharacters(offset)
             .then(this.onCharListLoaded)
             .catch(this.onError)
     }
 
-    onCharListLoaded = (charList) => {
+    onCharListLoading = () => {
         this.setState({
-            charList,
-            loading: false
+            newItemsLoading: true
         })
+    }
+
+    onCharListLoaded = (newcharList) => {
+        let ended = false
+        if (newcharList.length < 9) {
+            ended = true
+        }
+        
+        this.setState(({offset, charList}) => ({
+            charList: [...charList, ...newcharList],
+            loading: false,
+            newItemsLoading: false,
+            offset: offset + 9,
+            charEnded: ended
+        }))
     }
 
     onError = () => {
@@ -39,7 +61,7 @@ class CharList extends Component {
 
     // Этот метод создан для оптимизации, 
     // чтобы не помещать такую конструкцию в метод render
-
+    // треба зробити активний клас в елементів
     renderItems(arr) {
         const items = arr.map((item) => {
             let imgStyle = {'objectFit' : 'cover'}
@@ -65,7 +87,7 @@ class CharList extends Component {
     }
 
     render() {
-        const {charList, loading, error} = this.state
+        const {charList, loading, error, offset, newItemsLoading, charEnded} = this.state
 
         const items = this.renderItems(charList)
 
@@ -78,7 +100,11 @@ class CharList extends Component {
                 {errorMessage}
                 {spinner}
                 {content}
-                <button className="button button__main button__long">
+                <button
+                    className="button button__main button__long"
+                    disabled={newItemsLoading}
+                    style={{'display': charEnded ? 'none' : 'block'}}
+                    onClick={() => this.onRequest(offset)}>
                     <div className="inner">load more</div>
                 </button>
             </div>
